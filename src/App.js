ff2c16f3dd63d2b23819d639ecdb3ef60a4f25b8
Leon Lee <leon@Leons-MacBook-Pro.local>
2024-01-15 19:44:31 -0500

    added TopNavbar js and module css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './TopNavbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
---
+++ src/TopNavbar.js
@@ -0,0 +1,24 @@
+import styles from './TopNavbar.module.css'
+
+export default function Navbar() {
+    return (
+        <div className={styles.container}>
+            <nav className = {styles.nav}>
+                <ul>
+                    <li>
+                        <a>Leon Lee</a>
+                    </li>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#Contacts">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
