c5a1d8d692ad09ff84132480330f24a56e1673b6
TackWooLee-Leon <tackwlee01@gmail.com>
2024-02-14 17:26:07 -0500

    Add svg and id

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,7 +2,7 @@ import styles from './Contact.module.css'
 
 export default function Contact () {
     return(
-        <div className={styles.contactContainer}>
+        <div id="contacts" className={styles.contactContainer}>
             <h1 className={styles.contactHeader}>Contact Me</h1>
             <a href="mailto:tackwlee01@gmail.com"><img src="/images/gmail.svg" alt="gmail logo"></img></a>
 
