a843dcadc2bba5b89e921cb6bd34eb2eca6c9421
TackWooLee-Leon <tackwlee01@gmail.com>
2024-02-21 02:21:47 -0500

    Restyle span element, add hover animation

--- src/Components/ProjectsList.js
+++ src/Components/ProjectsList.js
@@ -1,14 +1,14 @@
 const projectsList = [
   {
     name: 'Math Wordle',
-    description: 'This Math Wordle game is similar to the popular game Wordle...',
+    description: 'This project is a math puzzle game similar to Wordle. where players aim to create a calculation matching a target output using digits 0-9 and basic operators within limited attempts.',
     src: '/images/math-wordle.png',
     alt: 'Math Wordle',
     href: 'https://github.com/TackWooLee-Leon/Math-Wordle'
   },
   {
     name: 'Budget App',
-    description: 'This is a budget app that calculates how much money you would have after you entered your expenses...',
+    description: 'A budget app where users input their budget and expenses. It calculates and displays the remaining balance after each expense. Users can remove their expenses as well.',
     src: '/images/budget-app.png',
     alt: "Budget App",
     href: ''
