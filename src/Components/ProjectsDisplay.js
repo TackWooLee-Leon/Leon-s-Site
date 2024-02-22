8426b0ac8b90873488710b163af4c05ddfc59e51
TackWooLee-Leon <tackwlee01@gmail.com>
2024-02-21 20:35:14 -0500

    Download AOS npm, add scroll trigger animation

--- src/Components/ProjectsDisplay.js
+++ src/Components/ProjectsDisplay.js
@@ -1,13 +1,16 @@
 import styles from './ProjectsDisplay.module.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 
 export default function ProjectsDisplay({info}) {
     console.log(info);
     return (
         <section id="projects" className={styles.projectContainer}>
-            <div className={`${styles.reveal} ${styles.projectInformation}`}>
+            <div data-aos-duration="700" data-aos="fade-up" className={styles.projectInformation}>
                     <img className={styles.projectImg} src={info.src} alt={info.alt}></img>
                 
-                <div className={`${styles.reveal} ${styles.projectDescriptionWrapper}`}>
+                <div className={styles.projectDescriptionWrapper}>
                     <h1 className={styles.projectName}>{info.name}</h1>
                     <p> {info.description} </p>
                     <div className={styles.repo}>
