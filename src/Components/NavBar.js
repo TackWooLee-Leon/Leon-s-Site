3bceac622180f1c904ebec9a68b3e9dc43914860
TackWooLee-Leon <tackwlee01@gmail.com>
2024-02-24 12:32:44 -0500

    minor changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -12,18 +12,20 @@ export default function NavBar() {
     <header>
     
       <nav>
+        {/* sidebar */}
         <ul className={isSidebarOpen ? styles.sidebarOpen : styles.sidebar}>
             <li className={styles.buttons} onClick={toggleSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" fill="#FFFADE" width="50"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contacts">Contacts</a></li>
+            <li onClick={toggleSidebar}><a href="#about">About</a></li>
+            <li onClick={toggleSidebar}><a href="#projects">Projects</a></li>
+            <li onClick={toggleSidebar}><a href="#contacts">Contacts</a></li>
         </ul>
+        {/* top navbar */}
         <div className={styles.navContainer}>
             <ul>
                 <li><a>Leon Lee</a></li>
-                <li className={styles.hideOnMobile}><a href="#contacts">Contacts</a></li>
                 <li className={styles.hideOnMobile}><a href="#about">About</a></li>
                 <li className={styles.hideOnMobile}><a href="#projects">Projects</a></li>
+                <li className={styles.hideOnMobile}><a href="#contacts">Contacts</a></li>
                 <li className={`${styles.buttons} ${styles.hideOnPC}`} onClick={toggleSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" fill="#FFFADE" width="50"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             </ul>
         </div>
