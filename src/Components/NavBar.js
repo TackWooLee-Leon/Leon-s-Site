593211630f6d19e309a7cac0b0406d71b5261505
TackWooLee-Leon <tackwlee01@gmail.com>
2024-02-23 22:51:39 -0500

    Make navbar responsive

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -12,7 +12,8 @@ export default function NavBar() {
     <header>
     
       <nav>
-        <ul className={styles.sidebar}>
+        <ul className={isSidebarOpen ? styles.sidebarOpen : styles.sidebar}>
+            <li className={styles.buttons} onClick={toggleSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" fill="#FFFADE" width="50"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#contacts">Contacts</a></li>
@@ -20,10 +21,10 @@ export default function NavBar() {
         <div className={styles.navContainer}>
             <ul>
                 <li><a>Leon Lee</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contacts">Contacts</a></li>
-                <li onClick={toggleSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" fill="#FFFADE" width="50"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+                <li className={styles.hideOnMobile}><a href="#contacts">Contacts</a></li>
+                <li className={styles.hideOnMobile}><a href="#about">About</a></li>
+                <li className={styles.hideOnMobile}><a href="#projects">Projects</a></li>
+                <li className={`${styles.buttons} ${styles.hideOnPC}`} onClick={toggleSidebar}><a><svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" fill="#FFFADE" width="50"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             </ul>
         </div>
       </nav>
