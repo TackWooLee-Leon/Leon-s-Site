abfe4bba6409d8ae43dcbc93912fc8d85ff53941
TackWooLee-Leon <tackwlee01@gmail.com>
2024-09-18 21:09:00 -0400

    Add flex-wrap

--- src/ProjectSection.js
+++ src/ProjectSection.js
@@ -7,10 +7,9 @@ AOS.init();
 
 
 export default function ProjectSection({info}) {
-    console.log(info);
     return (
       <div className={styles.projectSection}>
-            <h1 data-aos="fade-right" data-aos-duration="500" id="projects" className={styles.projectHeader}>Projects</h1>
+          <h1 data-aos="fade-right" data-aos-duration="500" id="projects" className={styles.projectHeader}>Projects</h1>
           <div className={styles.projectsGrid}>
             {projectsList.map((item)=> {
             return (<ProjectsDisplay info={item} key={item.name}/>)
